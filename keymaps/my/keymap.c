2e5fdfd4520835e57ca5200b50a587a3ce2bdfd4
Andrew Vorontsov <avv092@gmail.com>
2024-06-21 01:02:59 +0300

    fix layot

--- keymaps/my/keymap.c
+++ keymaps/my/keymap.c
@@ -12,16 +12,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [1] = LAYOUT(
-        KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS,        KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        KC_ESC, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,                          KC_PGUP, KC_HOME, KC_UP, KC_END, KC_TRNS, KC_BSLS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS,                          KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS 
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS,        KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                          KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_TRNS, KC_BSLS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                          KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_DEL,  KC_TRNS, KC_TRNS 
     ),
 
     [2] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        _______, _______, _______, _______, _______, _______,                          _______, _______, KC_UP, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                          _______, _______, KC_UP,   _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                          _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______,
                                             _______, _______, _______,        _______, _______, _______, _______, _______
